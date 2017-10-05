d2ebe860aa5fc764df1f0daed0568b937be25a06
Anton Österberg <anton.osterberg@gmail.com>
2017-10-05 14:40:21 +0200

    Removes import that was not needed and make build fail.

--- src/test/kotlin/JwtSyntaxTest.kt
+++ src/test/kotlin/JwtSyntaxTest.kt
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import se.zensum.jwt.isValidJwtSyntax
-import se.zensum.jwt.jwtRegex
 
 const val validToken: String = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.EkN-DOsnsuRjRO6BxXemmJDm3HbxrbRzXglbN2S4sOkopdU4IsDxTI8jO19W_A4K8ZPJijNLis4EZsHeY559a4DFOd50_OqgHGuERTqYZyuhtF39yxJPAjUESwxk2J5k_4zM3O-vtd1Ghyo4IbqKKSy6J9mTniYJPenn5-HIirE"
 const val validShortToken: String = "aaa.bbb.cc-c_c"
